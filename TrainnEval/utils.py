a0152b211b43f6bd40a0d6d2ea056b8e50b40cc9
Gitrushikesh19 <kaveeshwarrushikesh@gmail.com>
2025-09-22 23:16:39 +0530

    Helping functions for training

---
+++ TrainnEval/utils.py
@@ -0,0 +1,31 @@
+import torch
+from Config import config
+from torchvision.utils import save_image
+
+def save_examples(gen, val_loader, epoch, folder):
+    x, y = next(iter(val_loader))
+    x, y = x.to(config.Device), y.to(config.Device)
+    gen.eval()
+    with torch.no_grad():
+        y_fake = gen(x)
+        y_fake = y_fake*0.5 + 0.5 # Removing Normalization
+        save_image(y_fake, folder+f'/y_gen_{epoch}.png')
+        save_image(x*0.5 + 0.5, folder+f'/input_{epoch}.png')
+        if epoch == 1:
+            save_image(y*0.5 + 0.5, folder+f'label_{epoch}.png')
+    gen.train()
+
+def save_checkpoint(model, optimizer, filename='my_checkpoint.pth.tar'):
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    checkpoint = torch.load(checkpoint_file, map_location=config.Device)
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+    for param_grp in optimizer.param_groups:
+        param_grp['lr'] = lr
